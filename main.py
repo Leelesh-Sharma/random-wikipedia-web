c1282d2fb714980f5075a07ff63baeedb403d310
Leelesh Sharma <60009872+Leelesh-Sharma@users.noreply.github.com>
2021-08-07 13:38:34 +0530

    final

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import wikipedia
 
-while True:
-    random = wikipedia.random(1)
-    search = input(f'Do you want to learn about the {random}. (Y/N/Q)').lower()
-    if search =='q':
-        break
-    if search == 'n':
-        continue
-    elif search == 'y':
-        summary = wikipedia.summary(random, sentences=5)
-        print("\n"+summary+"\n")
-    else:
-        continue
+def random_wiki_searches():
+    while True:
+        random = wikipedia.random(1)
+        search = input(f'Do you want to learn about the {random}. (Y/N/Q)').lower()
+        if search =='q':
+            print('QUIT.')
+            break
+        if search == 'n':
+            continue
+        elif search == 'y':
+            summary = wikipedia.summary(random, sentences=5)
+            print("\n"+summary+"\n")
+        else:
+            print("\nInvalid action.")
+            continue
+            
 
-print('QUIT.')
+random_wiki_searches()
